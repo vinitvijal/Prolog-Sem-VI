650e671af644497c15b046b49ed2fe8673ce279d
Vinu <vineet.22566@sscbs.du.ac.in>
2025-05-05 20:55:01 +0530

    Q1

---
+++ Q1.pl
@@ -0,0 +1,20 @@
+male(jawaharlal).
+male(rajiv).
+male(feroz).
+female(indira).
+female(priyanka).
+male(rahul).
+
+
+parent(jawaharlal, indira).
+parent(indira, rajiv).
+parent(indira, sanjay).
+parent(rajiv, rahul).
+parent(rajiv, priyanka).
+parent(feroz, rajiv).
+
+
+father(X, Y) :- parent(X, Y), male(X).
+mother(X, Y) :- parent(X, Y), female(Y).
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
